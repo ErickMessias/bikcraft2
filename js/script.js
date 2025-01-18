222c15ae0f992e5d14de9e5c7c5084b84501ed8a
Erick Messias <erick.messias182@gmail.com>
2025-01-18 14:32:29 -0300

    adicionando-js

--- js/script.js
+++ js/script.js
@@ -44,4 +44,27 @@ function eventosPerguntas(pergunta) {
 
 perguntas.forEach(eventosPerguntas);
 
-console.log(perguntas);
+//galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  console.log(media);
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
